12cddaf1f35e12e63b4373d679aaf19cea785766
Isaiah Schultz <imschultz@lincl10.cs.csbsju.edu>
2012-10-23 15:28:03 -0500

    Fixed the crash when loggin in with no username or password

--- edu.csbsju.whats-happenin-cs330/src/edu/csbsju/whats/happenin/Login.java
+++ edu.csbsju.whats-happenin-cs330/src/edu/csbsju/whats/happenin/Login.java
@@ -3,14 +3,16 @@ package edu.csbsju.whats.happenin;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeoutException;
 
-import edu.csbsju.whats.happenin.dataAccess.SQLHelper;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+import edu.csbsju.whats.happenin.dataAccess.SQLHelper;
 
 public class Login extends Activity {
 
@@ -33,21 +35,36 @@ public class Login extends Activity {
     	EditText pswdText = (EditText)findViewById(R.id.password);
     	String password = pswdText.getText().toString();
     	
-    	TextView tv = (TextView) findViewById(R.id.errorMsg);
-    	
     	//SQL Query: SELECT * FROM Users u where u.username = username AND u.password = password
     	
-    	User user = SQLHelper.getUserByUsername(username);
+    	User user = null;
     	
-    	if(user == null){
-    		tv.setText("Wrong info");
+    	if(username.equals("") || password.equals("")){
+    		Context context = getApplicationContext();
+    		CharSequence text = "Please include both your username and password";
+    		int duration = Toast.LENGTH_SHORT;
+    		Toast.makeText(context, text, duration).show();
     	}
-    	else if(password.equals(user.getPassword()) && username.equals(user.getUsername())){
-    		Intent intent = new Intent(this, ViewHappenins.class);
-    		startActivity(intent);
-    	} else 
-    		tv.setText("Wrong shit");
-    		
+    	else {
+    		user = SQLHelper.getUserByUsername(username);
+
+    		////note to Isaiah: tidy up this toast shit
+    		if(user == null){
+    			Context context = getApplicationContext();
+    			CharSequence text = "Incorrect login info provided";
+    			int duration = Toast.LENGTH_SHORT;
+    			Toast.makeText(context, text, duration).show();
+    		}
+    		else if(password.equals(user.getPassword()) && username.equals(user.getUsername())){
+    			Intent intent = new Intent(this, ViewHappenins.class);
+    			startActivity(intent);
+    		} else {
+    			Context context = getApplicationContext();
+    			CharSequence text = "Incorrect login info provided";
+    			int duration = Toast.LENGTH_SHORT;
+    			Toast.makeText(context, text, duration).show();
+    		}
+    	}
     	
     }
 }
