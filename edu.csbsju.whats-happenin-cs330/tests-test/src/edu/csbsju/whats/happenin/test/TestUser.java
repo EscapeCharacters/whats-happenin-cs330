7976ab9c0de27592fd62a8a21b5b0e820c313065
Andrew Zurn <andyzurn@gmail.com>
2012-11-19 13:46:55 -0600

    Test package has been added, work on test (TestUser) began.

---
+++ edu.csbsju.whats-happenin-cs330/tests-test/src/edu/csbsju/whats/happenin/test/TestUser.java
@@ -0,0 +1,71 @@
+package edu.csbsju.whats.happenin.test;
+
+import junit.framework.TestCase;
+
+public class TestUser extends TestCase {
+
+	public TestUser(String name) {
+		super(name);
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	public void testUser() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public void testUserStringStringStringString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public void testGetUsername() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public void testSetUsername() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public void testGetPassword() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public void testSetPassword() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public void testSetUserId() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public void testGetUserId() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public void testSetEmailAddress() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public void testGetEmailAddress() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public void testGetName() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public void testSetName() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public void testToString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
