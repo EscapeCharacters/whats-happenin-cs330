9aed19593dfcd82c78281ee9cb616cb7577eee37
Andrew J. Thom <ajthom@csbsju.edu>
2012-11-20 11:59:57 -0600

    Added documentation to Login screen

--- edu.csbsju.whats-happenin-cs330/src/edu/csbsju/whats/happenin/Login.java
+++ edu.csbsju.whats-happenin-cs330/src/edu/csbsju/whats/happenin/Login.java
@@ -14,6 +14,11 @@ import android.widget.EditText;
 import android.widget.Toast;
 import edu.csbsju.whats.happenin.dataAccess.SQLHelper;
 
+/**
+* This is the login screen activity!  This is the first thing someone sees when they load the app
+* @author EscapeCharacters
+* @version 11/20/2012
+*/
 public class Login extends Activity {
 
 	@Override
@@ -29,11 +34,17 @@ public class Login extends Activity {
 		return true;
 	}
 	
-	public void newUser(View view)throws InterruptedException, ExecutionException, TimeoutException{
+	/**
+	* Handles when someone presses the Create User button on the login screen
+	*/
+	public void newUser(View view) throws InterruptedException, ExecutionException, TimeoutException{
 		Intent intent = new Intent(this, CreateNewUser.class);
 		startActivity(intent);
 	}
 	
+	/**
+	* Handles when someone fills out the login screen and clicks Login
+	*/
 	public void login(View view) throws InterruptedException, ExecutionException, TimeoutException{
 		EditText userText = (EditText)findViewById(R.id.username);
 		String username = userText.getText().toString();
@@ -41,27 +52,31 @@ public class Login extends Activity {
 		EditText pswdText = (EditText)findViewById(R.id.password);
 		String password = pswdText.getText().toString();
 
-		//SQL Query: SELECT * FROM Users u where u.username = username AND u.password = password
-
 		User user = null;
 
+		// Checks if both fields are filled in
 		if(username.equals("") || password.equals("") ){
 			toastErrorMsg("Please include both your username and password!");
 		}
 		else {
 			try{
+				// Gets user from DB
 				user = SQLHelper.getUserByUsername(username);
 				if(user == null){
 					toastErrorMsg("Incorrect login info provided!");
 				}
+				//If good, then it logs you in
 				else if(password.equals(user.getPassword()) && username.equals(user.getUsername())){
 					Intent intent = new Intent(this, ViewHappenins.class);
 					startActivity(intent);
-				} else {
+				} 
+				// If not good, then provides error message
+				else {
 					toastErrorMsg("Incorrect login info provided!");
 				}
 			} catch(Exception e){
-				toastErrorMsg("onnection to What's Happenin' unavailable! Please check your internet connection and try again later!");
+				// This is executed if internet connection is unavailable
+				toastErrorMsg("Connection to What's Happenin' unavailable! Please check your internet connection and try again later!");
 			}
 		}
 	}
@@ -78,6 +93,9 @@ public class Login extends Activity {
 		startActivity(intent);
 	}
 	
+	/**
+	* Helper method to create a 'Toast'
+	*/
 	public void toastErrorMsg(String error){
 		Context context = getApplicationContext();
 		CharSequence text = error;
