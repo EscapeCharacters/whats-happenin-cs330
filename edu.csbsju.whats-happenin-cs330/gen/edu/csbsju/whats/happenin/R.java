1c0247d8ffa40f0069a2c656082dc18a71447ef5
Andrew Thom <ajthom@csbsju.edu>
2012-12-13 12:14:11 -0600

    Updated the change password layout so the old password field is a password field

--- edu.csbsju.whats-happenin-cs330/gen/edu/csbsju/whats/happenin/R.java
+++ edu.csbsju.whats-happenin-cs330/gen/edu/csbsju/whats/happenin/R.java
@@ -33,6 +33,7 @@ public final class R {
     public static final class drawable {
         public static final int ic_action_search=0x7f020000;
         public static final int ic_launcher=0x7f020001;
+        public static final int ic_launcher1=0x7f020002;
     }
     public static final class id {
         public static final int change_password_confirm_new_password=0x7f090002;
